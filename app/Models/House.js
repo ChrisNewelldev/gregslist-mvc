6ed4878b095cd40aae67634ff5877d26375bcd36
thedudeman69 <christopher.jp.newell@gmail.com>
2021-07-14 14:28:06 -0600

    houses form added to html

--- app/Models/House.js
+++ app/Models/House.js
@@ -1,3 +1,17 @@
 export default class house {
   
+  constructor ({ beds, baths, squarefeet, acres, price, imgUrl }) {
+    this.beds = beds
+    this.baths = baths
+    this.squarefeet = squarefeet
+    this.acres = acres
+    this.price = price
+    this.description = description || "no house description"
+    this.imgUrl = imgUrl || '//placehold.it/200x200'
+  }
+
+
+get template() {
+
+}
 }
