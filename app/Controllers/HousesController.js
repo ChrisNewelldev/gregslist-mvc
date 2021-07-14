35eb00dcedbf8cb96986a822638f3c0352407a43
thedudeman69 <christopher.jp.newell@gmail.com>
2021-07-14 15:19:49 -0600

    houses controller complete for now

--- app/Controllers/HousesController.js
+++ app/Controllers/HousesController.js
@@ -1,2 +1,34 @@
 import { ProxyState } from "../AppState.js";
-import {HousesService} from "../"
+import { housesService } from "../Services/HousesService.js"
+
+function _draw() {
+  let template = ''
+  ProxyState.houses.forEach(house => {
+    template += house.template
+  })
+  document.getElementById('houses').innerHTML = template
+}
+
+export default class HousesController {
+  constructor(){
+  ProxyState.on('houses', _draw)
+  ProxyState.on('houses', () => (console.log('new house') ))
+_draw()
+}
+
+createHouse () {
+  event.preventDefault()
+  let form = event.target
+  let rawHouse = {
+    beds: form.beds.value,
+    baths: form.baths.value,
+    squarefeet: form.squarefeet.value,
+    acres: form.acres.value,
+    price: form.prices.value,
+    description: form.description.value,
+    imgUrl: form.imgUrl.value
+  }
+  housesService.createHouse(rawHouse)
+  form.reset
+}
+}
