78a7671908f4791e075ad11e3532ac24bdf012bd
thedudeman69 <christopher.jp.newell@gmail.com>
2021-07-20 15:49:42 -0600

    houses updated

--- app/Services/HousesService.js
+++ app/Services/HousesService.js
@@ -1,10 +1,54 @@
 import { ProxyState } from "../AppState.js"
 import House from "../Models/House.js"
+import { api } from './AxiosService.js'
+
 
 class HousesService {
-  createHouse(rawHouse) {
-    debugger
-    ProxyState.House = [...ProxyState.houses, new House(rawHouse)]
+  
+  constructor(){
+    this.getAllHouses()
+  }
+
+  async createHouse(rawHouse) {
+    console.log('creating house step 2')
+    const res = await api.post('houses', rawHouse)
+    console.log('your new house sir',res.data)
+    console.log('creaing house step 3')
+    ProxyState.houses = [...ProxyState.houses, new House(res.data)]
+  }
+
+  async getAllHouses(){
+    try {
+      const res = await api.get('houses')
+      console.log(res.data)
+      ProxyState.houses = res.data.map(h => new House(h))
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async bidHouse(houseId) {
+    try {
+      let foundHouse = ProxyState.houses.find(h => h.id == houseId)
+      foundHouse.price += 100000
+      const res = await api.put('houses/'+ houseId, foundHouse)
+      console.log('updated house', res.data)
+
+      ProxyState.houses = ProxyState.houses
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async deleteHouse(houseId) {
+    try {
+      const res = await api.delete('houses/'+ houseId)
+      console.log(res.data)
+      ProxyState.houses = ProxyState.houses.filter( h => h.id != houseId)
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
 
